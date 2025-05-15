4acd2da33c153474deb6c001290e922a123a2724
Maria Jernström <maria.jernstrom@eqtpartners.com>
2025-05-15 09:26:11 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <SectorBubbleChart>
    
    with the text content:
    
        [data-chart=chart-«Rqgtl7»] {
          --color-tech_bubble: hsl(var(--chart-1));
          --color-market_insights_bubble: hsl(var(--chart-2));
          --color-finance_bubble: hsl(var(--chart-3));
          --color-sector4: hsl(var(--chart-3));
          --color-sector5: hsl(var(--chart-3));
          --color-sector6: hsl(var(--chart-3));
        }
    
        .dark [data-chart=chart-«Rqgtl7»] {
          --color-tech_bubble: hsl(var(--chart-1));
          --color-market_insights_bubble: hsl(var(--chart-2));
          --color-finance_bubble: hsl(var(--chart-3));
          --color-sector4: hsl(var(--chart-3));
          --color-sector5: hsl(var(--chart-3));
          --color-sector6: hsl(var(--chart-3));
        }
        010203040Private Company Performance Index036912Public Company Performance IndexTelecom...Softwar...Media&...BanksTelecom...Insurance
    
    Relevant files:
    
        - https://6000-firebase-studio-1747211323072.cluster-ombtxv25tbd6yrjpp3lukp6zhc.cloudworkstations.dev/_next/static/chunks/node_modules_a31e2e15._.js
        - https://6000-firebase-studio-1747211323072.cluster-ombtxv25tbd6yrjpp3lukp6zhc.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
        - https://6000-firebase-studio-1747211323072.cluster-ombtxv25tbd6yrjpp3lukp6zhc.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
    
    And change it as follows:
    
    Adjust the Y axis to cover the range of 0 to 40

--- src/lib/chart-config.ts
+++ src/lib/chart-config.ts
@@ -4,27 +4,51 @@ import type { BubbleChartDataPoint } from '@/lib/types';
 export const bubbleChartData: BubbleChartDataPoint[] = [
   { 
     id: 'tech_bubble', 
-    name: 'Transportation', 
-    privatePerformance: 26, 
-    publicPerformance: 10, 
+    name: 'Telecommunication Services', 
+    privatePerformance: 38, 
+    publicPerformance: 12, 
     relativeStrength: 120, 
     fill: 'hsl(var(--chart-1))' 
   },
   { 
     id: 'market_insights_bubble', 
-    name: 'Media & Entertainment', 
+    name: 'Software & Services', 
     privatePerformance: 24, 
-    publicPerformance: 12, 
+    publicPerformance: 8, 
     relativeStrength: 90, 
     fill: 'hsl(var(--chart-2))' 
   },
   { 
     id: 'finance_bubble', 
-    name: 'Software Services', 
-    privatePerformance: 19, 
-    publicPerformance: 8, 
+    name: 'Media & Entertainment', 
+    privatePerformance: 35, 
+    publicPerformance: 12, 
     relativeStrength: 105, 
     fill: 'hsl(var(--chart-3))' 
   },
+  { 
+    id: 'sector4', 
+    name: 'Banks', 
+    privatePerformance: 29, 
+    publicPerformance: 12, 
+    relativeStrength: 105, 
+    fill: 'hsl(var(--chart-3))' 
+  },
+  { 
+    id: 'sector5', 
+    name: 'Telecommunication Services', 
+    privatePerformance: 28, 
+    publicPerformance: 12, 
+    relativeStrength: 105, 
+    fill: 'hsl(var(--chart-3))' 
+  },
+  { 
+    id: 'sector6', 
+    name: 'Insurance', 
+    privatePerformance: 19, 
+    publicPerformance: 12, 
+    relativeStrength: 105, 
+    fill: 'hsl(var(--chart-3))' 
+  }
   // You can add or modify more data points here
 ];
